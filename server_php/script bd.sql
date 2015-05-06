6fe46f063b01c94846ff53b6591e4529d696b207
Alberto <afernandez268@ikasle.ehu.es>
2015-05-06 17:54:07 +0200

    Script BD añadido

---
+++ server_php/script bd.sql
@@ -0,0 +1,210 @@
+-- MySQL dump 10.13  Distrib 5.5.43, for Win64 (x86)
+--
+-- Host: localhost    Database: bdremota
+-- ------------------------------------------------------
+-- Server version	5.5.43
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `comentarios`
+--
+
+DROP TABLE IF EXISTS `comentarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `comentarios` (
+  `id_hist` int(11) NOT NULL,
+  `id_us` int(11) NOT NULL,
+  `texto` text,
+  `fecha` datetime NOT NULL,
+  PRIMARY KEY (`id_hist`,`id_us`,`fecha`),
+  KEY `id_us` (`id_us`),
+  CONSTRAINT `comentarios_ibfk_1` FOREIGN KEY (`id_hist`) REFERENCES `historia` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `comentarios_ibfk_2` FOREIGN KEY (`id_us`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comentarios`
+--
+
+LOCK TABLES `comentarios` WRITE;
+/*!40000 ALTER TABLE `comentarios` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comentarios` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `etiqueta`
+--
+
+DROP TABLE IF EXISTS `etiqueta`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `etiqueta` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(25) DEFAULT NULL,
+  `num` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `etiqueta`
+--
+
+LOCK TABLES `etiqueta` WRITE;
+/*!40000 ALTER TABLE `etiqueta` DISABLE KEYS */;
+/*!40000 ALTER TABLE `etiqueta` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `etiquetas`
+--
+
+DROP TABLE IF EXISTS `etiquetas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `etiquetas` (
+  `id_etiq` int(11) NOT NULL DEFAULT '0',
+  `id_hist` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id_etiq`,`id_hist`),
+  KEY `id_hist` (`id_hist`),
+  CONSTRAINT `etiquetas_ibfk_1` FOREIGN KEY (`id_etiq`) REFERENCES `etiqueta` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `etiquetas_ibfk_2` FOREIGN KEY (`id_hist`) REFERENCES `historia` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `etiquetas`
+--
+
+LOCK TABLES `etiquetas` WRITE;
+/*!40000 ALTER TABLE `etiquetas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `etiquetas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `historia`
+--
+
+DROP TABLE IF EXISTS `historia`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `historia` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_us` int(11) DEFAULT NULL,
+  `titulo` varchar(25) DEFAULT NULL,
+  `descripcion` text,
+  `fecha` datetime DEFAULT NULL,
+  `num_me_gusta` int(11) DEFAULT '0',
+  `num_no_me_gusta` int(11) DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `id_us` (`id_us`),
+  CONSTRAINT `historia_ibfk_1` FOREIGN KEY (`id_us`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `historia`
+--
+
+LOCK TABLES `historia` WRITE;
+/*!40000 ALTER TABLE `historia` DISABLE KEYS */;
+/*!40000 ALTER TABLE `historia` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `imagenes`
+--
+
+DROP TABLE IF EXISTS `imagenes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `imagenes` (
+  `id_hist` int(11) NOT NULL DEFAULT '0',
+  `path` varchar(45) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id_hist`,`path`),
+  CONSTRAINT `imagenes_ibfk_1` FOREIGN KEY (`id_hist`) REFERENCES `historia` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `imagenes`
+--
+
+LOCK TABLES `imagenes` WRITE;
+/*!40000 ALTER TABLE `imagenes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `imagenes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `suscripciones`
+--
+
+DROP TABLE IF EXISTS `suscripciones`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `suscripciones` (
+  `id_hist` int(11) NOT NULL DEFAULT '0',
+  `id_us` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id_hist`,`id_us`),
+  KEY `id_us` (`id_us`),
+  CONSTRAINT `suscripciones_ibfk_1` FOREIGN KEY (`id_hist`) REFERENCES `historia` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `suscripciones_ibfk_2` FOREIGN KEY (`id_us`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `suscripciones`
+--
+
+LOCK TABLES `suscripciones` WRITE;
+/*!40000 ALTER TABLE `suscripciones` DISABLE KEYS */;
+/*!40000 ALTER TABLE `suscripciones` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `contrasena` varchar(100) DEFAULT NULL,
+  `id_gcm` varchar(25) DEFAULT NULL,
+  `avisar_nuevo_historia` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-05-06 17:52:13
