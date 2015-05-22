15035d431e62a3cdddb2557766a474338dadb79d
davidramirez <davidramirez_28@hotmail.com>
2015-05-22 09:23:15 +0200

    Actualizo interfaces nueva y ver historia con los controles del slider, quito en menu en nueva y colores de fondo

--- app/src/main/java/org/das/das_grupo/NuevaHistoriaActivity.java
+++ app/src/main/java/org/das/das_grupo/NuevaHistoriaActivity.java
@@ -252,7 +252,7 @@ public class NuevaHistoriaActivity extends ActionBarActivity implements Selector
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_nueva_historia, menu);
+        //getMenuInflater().inflate(R.menu.menu_nueva_historia, menu);
         return true;
     }
 
